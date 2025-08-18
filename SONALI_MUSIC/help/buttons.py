9618a2276fabdd943932506305c5a3cf9eb661cd
TEAM KRITI BOTS <rejemtahir@gmail.com>
2025-08-19 00:00:54 +0530

    Update buttons.py

--- SONALI_MUSIC/help/buttons.py
+++ SONALI_MUSIC/help/buttons.py
@@ -101,7 +101,7 @@ class BUTTONS(object):
         ]
     PBUTTON = [
         [
-            InlineKeyboardButton("˹ ᴄσηᴛᴧᴄᴛ ˼", url="https://t.me/lll_BADNAM_BABY_lll")
+            InlineKeyboardButton("˹ ᴋɪʀᴛɪ ꭙ ʙᴏᴛѕ ˼", url="https://t.me/lll_BADNAM_BABY_lll")
         ],
         [
             InlineKeyboardButton("⌯ ʙᴧᴄᴋ ⌯", callback_data="MAIN_CP"),
