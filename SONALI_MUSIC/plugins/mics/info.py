aa9f42b0b517241d55326c10c6c5558ca5bb7dcd
TEAM KRITI BOTS <rejemtahir@gmail.com>
2025-08-28 22:00:26 +0530

    Update info.py

--- SONALI_MUSIC/plugins/mics/info.py
+++ SONALI_MUSIC/plugins/mics/info.py
@@ -11,7 +11,7 @@ from typing import Union, Optional
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Hena_music?startgroup=true"),
+        InlineKeyboardButton(text="✙ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ✙", url=f"https://t.me/Hena_robot?startgroup=true"),
     ],
 ]
 
